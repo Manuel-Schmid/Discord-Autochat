0d161e3c6f587ef2ab62e12c5eb02215bd4ce78e
Manuel Schmid <manysch3@gmail.com>
2021-10-09 22:45:24 +0200

    added code to send content from own account

--- MessageSender.py
+++ MessageSender.py
@@ -1,2 +1,13 @@
 import requests
 
+# https://discord.com/api/v9/channels/700261153690877976/messages
+
+payload = {
+    'content': "hello world"
+}
+
+header = {
+    'authorization': 'token goes here'
+}
+
+r = requests.post("https://discord.com/api/v9/channels/700261153690877976/messages", data=payload, headers=header)
